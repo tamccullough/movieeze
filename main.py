eb801cb9a5ca20292209408222e967b7439bee92
tamccullough <howdytodd@gmail.com>
2020-05-06 08:50:33 -0400

    updated code base

--- main.py
+++ main.py
@@ -8,23 +8,35 @@ current_year = date.today().strftime('%Y')
 import numpy as np
 import pandas as pd
 
+genres = ['Action','Adventure','Animation',
+          'Children','Comedy','Crime',
+          'Documentary','Drama','Fantasy',
+          'Film-Noir','Horror','Musical',
+          'Mystery','Romance','Sci-Fi',
+          'Thriller','War','Western']
+
 movieeze = Flask(__name__)
 
 @movieeze.route('/index')
 def index():
     #get the ingredients and search
-    return render_template('/index.html')#, posts=posts)
+    return render_template('index.html', genres = genres)
 
 @movieeze.route('/movies', methods=['POST'])
 #@login_required
 def movies():
     #posting the results
-    user_id = 4168
-    genres_list = request.form['genres']
-    movies_list = ml_main.set_up_ml(user_id,genres_list)
-    #movies_list = ml_main.set_test()
-    num_ = movies_list.shape[0]
-    return render_template('/movies.html',movies_table = movies_list,g_list = genres_list)
+    user_1 = 123711 # user with a medium amount of ratings
+    user_2 = 15078 # user with the lowest number of ratings
+    user_3 = 72315 # user with a lot of ratings
+    genres_list = request.form['Genres']
+
+    recommended_list_1 = ml_main.set_up_ml(user_1,genres_list) # generate a list of recommendations for each user
+    recommended_list_2 = ml_main.set_up_ml(user_2,genres_list)
+    recommended_list_3 = ml_main.set_up_ml(user_3,genres_list)
+
+    final_recommendation = ml_main.get_final_recommendation(recommended_list_1,recommended_list_2,recommended_list_3)
+    return render_template('movies.html',movies_table = final_recommendation,genres = genres_list)
 
 
 if __name__ == "__main__":
--- ml_main.py
+++ ml_main.py
@@ -5,7 +5,7 @@ import heapq
 import pickle
 import os
 
-filename = 'model/movielens_light_recommender_model.sav'
+filename = 'model/moviEeze_recommender_model_2010_up.sav'
 ml_model = pickle.load(open(filename, 'rb'))
 
 genres = ['Action','Adventure','Animation',
@@ -15,9 +15,9 @@ genres = ['Action','Adventure','Animation',
           'Mystery','Romance','Sci-Fi',
           'Thriller','War','Western']
 
-movies = pd.read_csv('datasets/movies.csv')
-ratings = pd.read_csv('datasets/ratings.csv')
-users = pd.read_csv('datasets/users.csv')
+movies = pd.read_csv('datasets/movies_2010.csv')
+ratings = pd.read_csv('datasets/ratings_2010.csv')
+links = pd.read_csv('datasets/links_2010.csv')
 
 def get_r(user_id):
     # Select which system to use. Due to memory constraints, item based is the only viable option
@@ -26,12 +26,12 @@ def get_r(user_id):
     N = 2000
 
     # The setting to a set and back to list is a failsafe.
-    rated_items = list(set(ratings.loc[ratings['userid'] == user_id]['movieid'].tolist()))
-    ratings_list = movies['movieid'].values.tolist()
-    reduced_ratings = ratings.loc[ratings['movieid'].isin(ratings_list)].copy()
+    rated_items = list(set(ratings.loc[ratings['userId'] == user_id]['movieId'].tolist()))
+    ratings_list = movies['movieId'].values.tolist()
+    reduced_ratings = ratings.loc[ratings['movieId'].isin(ratings_list)].copy()
 
     # Self explanitory name
-    all_item_ids = list(set(reduced_ratings['movieid'].tolist()))
+    all_item_ids = list(set(reduced_ratings['movieId'].tolist()))
 
     # New_items just represents all the items not rated by the user
     new_items = [x for x in all_item_ids if x not in rated_items]
@@ -47,7 +47,7 @@ def get_r(user_id):
     recommended_ids = sorted(recommended_ids)
 
     # predicted_ratings
-    recommended_df = movies.loc[movies['movieid'].isin(recommended_ids)].copy()
+    recommended_df = movies.loc[movies['movieId'].isin(recommended_ids)].copy()
     #recommended_df.insert(1, 'pred_rating', np.zeros(len(recommended_ids)))
     recommended_df.insert(1, 'pred_rating', 0)
 
@@ -64,7 +64,8 @@ def cap_str(item):
 def reg_frame(f_list,items):
     s_ = ''
     for i in items:
-        j = cap_str(i)
+        j = i.strip()
+        j = cap_str(j)
         str_ = f'(?=.*{j})'
         s_ += str_
     s_
@@ -72,34 +73,25 @@ def reg_frame(f_list,items):
     return f_list
 
 def set_up_ml(user_id,genre_list):
-    f_list = get_r(user_id)
+    film_list = get_r(user_id)
     items = genre_list.split(',')
-    f_list = reg_frame(f_list,items)
-    f_list.pop('movieid')
-    f_list.pop('pred_rating')
-    f_list = f_list.reset_index(drop=True)
-    f_list = f_list.T.reset_index(drop=True).T
-    #f_list = f_list.head(10)
-    return f_list
+    film_list = reg_frame(film_list,items)
+    film_list.pop('date')
+    return film_list
 
-
-# JUST KEEP IT HERE SAFELY
-def set_up_back(user_id,genre_list):
-    ml_list = get_r(user_id)
-    cols = ml_list.columns
-    f_list = pd.DataFrame(columns = cols)
-    items = genre_list.split(',')
-    s_ = ''
-    for i in items:
-        j = cap_str(i)
-        str_ = f'(?=.*{j})'
-        s_ += str_
-    s_
-    f_list = ml_list.copy()
-    f_list = f_list[f_list['genres'].str.contains(fr'^\b{s_}\b',regex=True)]
-    f_list.pop('movieid')
-    f_list.pop('pred_rating')
-    f_list = f_list.reset_index(drop=True)
-    f_list = f_list.T.reset_index(drop=True).T
-    f_list = f_list.head(10)
-    return f_list
+def get_final_recommendation(list_1,list_2,list_3): # combine all recommendations
+    film_recommendation = pd.DataFrame()
+    film_recommendation = pd.concat([list_1,list_2,list_3]) # concat lists
+    film_recommendation = film_recommendation.drop_duplicates() # drop recommended duplicates of films
+    film_recommendation = film_recommendation.sort_values('pred_rating',ascending=False) # sort by predicted rating
+    film_recommendation.pop('pred_rating') # drop the rating column
+    film_recommendation = film_recommendation.reset_index()
+    film_recommendation.pop('index') # reset and pop the old index
+    a = []
+    for i in range(0,film_recommendation.shape[0]): # iterate through the dataframe and get the appropriate link for each movie
+        link = links[links['movieId'] == film_recommendation.iloc[i]['movieId']]
+        link = 'https://www.themoviedb.org/movie/' + str(int(link.iloc[0][2])) # append the link to the array
+        a.append(link)
+    film_recommendation['link'] = a # add the array to the dataframe
+    film_recommendation.pop('movieId')
+    return film_recommendation
